ed9e1fd915d43c26efc348029646721ac5754c9c
urvashi <rawaturvashi68@gmail.com>
2022-11-27 22:10:37 +0530

    events & use of state hook

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,16 +1,26 @@
 // Component in React is just a Javascript Function which returns JSX
+import React, { useState } from "react";
+
 import "./ExpenseItem.css"; //styling components
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
-function ExpenseItem(props) {  
+function ExpenseItem(props) {
+  //one of the react hook
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    // console.log(props.title);
+    setTitle('hey')
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
