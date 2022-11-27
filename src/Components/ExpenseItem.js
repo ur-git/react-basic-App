f2b18c65d5bf51fe7a74f65e5724bce15f0e8f82
urvashi <rawaturvashi68@gmail.com>
2022-11-27 12:38:58 +0530

    splitting components

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,17 +1,11 @@
 // Component in React is just a Javascript Function which returns JSX
 import "./ExpenseItem.css"; //styling components
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const date = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
+function ExpenseItem(props) {  
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{date}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
